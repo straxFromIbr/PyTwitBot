3d3a607b7805f0f395206fcae8ba1c802548f17e
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-11-18 03:43:24 +0000

    use fugashi instead of py-mecab

--- src/markovbot.py
+++ src/markovbot.py
@@ -2,12 +2,10 @@ import logging
 import pathlib
 import random
 
-import MeCab
 import markovify
+from fugashi import fugashi
 
 import text_utils
-
-# parser = MeCab.Tagger("-d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd")
 
 
 def parse_text(text: str, parser) -> str:
@@ -15,22 +13,17 @@ def parse_text(text: str, parser) -> str:
     if filterd is None:
         filterd = text
 
-    parsed = parser.parse(filterd)
+    parsed = parser(filterd)
     if parsed is None:
         return " ".join(filterd.split())
-
-    words_list = [
-        morph.split("\t")[0]
-        for morph in parsed.split("\n")
-        if len(morph) != 0 and morph != "EOS"
-    ]
-
-    words = " ".join(words_list)
-    return words
+    res_text = ""
+    for w in parsed:
+        res_text += str(w) + " "
+    return res_text.strip()
 
 
 def gen_text(datapath: pathlib.Path) -> str:
-    parser = MeCab.Tagger()
+    parser = fugashi.Tagger()
     with open(datapath) as dataf:
         text_list = dataf.readlines()
 
@@ -48,5 +41,5 @@ def gen_text(datapath: pathlib.Path) -> str:
 
 
 if __name__ == "__main__":
-    path = pathlib.Path("./tweets.txt")
+    path = pathlib.Path("../data/tweets.txt")
     print(gen_text(path))
